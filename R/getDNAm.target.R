787af074c3733ad707e489499234a8f1004d4ce0
Tiago Chedraoui Silva <tiagochst@gmail.com>
2019-11-15 16:08:08 -0500

    update

--- R/getDNAm.target.R
+++ R/getDNAm.target.R
@@ -4,7 +4,7 @@
 #' @param genome Human genome of reference "hg38" or "hg19"
 #' @param method For the moment mapping region to closest gene
 #' @importFrom coMethDMR AnnotateResults
-#' @importFrom GenomicRanges distanceToNearest nearest ranges
+#' @importFrom GenomicRanges distanceToNearest nearest ranges makeGRangesFromDataFrame values
 #' @importFrom tidyr unite
 #' @importFrom ELMER getTSS
 #' @examples
@@ -34,13 +34,75 @@ getDNAm.target <- function(
                        distance.region.tss)
 
     colnames(neargenes)[1:3] <- c("gene_chrom","gene_start","gene_end")
-    regionID <- ranges(regions.gr) %>%
-        as.data.frame() %>%
-        tidyr::unite(col = "regionID")
-
-    region.df <- regions.gr %>% as.data.frame()
-    colnames(region.df)[1:3] <- c("region_chrom","region_start","region_end")
-
-    out <- cbind(region.df, neargenes)
+    regionID <- paste0( seqnames(regions.gr) %>% as.character(),
+                        ":",
+                        start(regions.gr),
+                        "-",
+                        end(regions.gr))
+    out <- cbind(regionID, neargenes)
     return(out)
+}
+
+
+#' @title Evaluate correlation of region and gene using spearman test
+#' @description Evaluate correlation of region and gene using spearman test
+#' @param links.df A dataframe with the following collumns
+#' region and gene
+#' @param met DNA methylation matrix (rows are regions, columns samples). Samples should be in the
+#' same order as gene expression.
+#' @param exp Gene expression matrix (rows are genes, columns samples)
+#' Samples should be in the same order as gene expression.
+#' @param file.out A csv file name to save the results.
+#' @importFrom plyr adply
+#' @export
+#' @examples
+#' regions.gr <- data.frame(
+#'  chrom = c("chr22", "chr22", "chr22", "chr22", "chr22"),
+#'   start = c("39377790", "50987294", "19746156", "42470063", "43817258"),
+#'   end   = c("39377930", "50987527", "19746368", "42470223", "43817384"),
+#'                          stringsAsFactors = FALSE)  %>%
+#'                          makeGRangesFromDataFrame
+#' map <- getDNAm.target(regions.gr = regions.gr ,genome = "hg19")
+#' map <- unite(map,col = "regionID",c("region_chrom", "region_start", "region_end" ))
+#' links <- tibble::tibble(regionID = map$regionID, geneID = map$ensembl_gene_id)
+#' met <- matrix(rep(0,length(links$regionID) * 4),
+#'               nrow = length(links$regionID),
+#'               dimnames = list(c(links$regionID),c(paste0("S",c(1:4)))))
+#'
+#' exp <- matrix(rep(0,length(links$geneID) * 4),
+#'               nrow = length(links$geneID),
+#'               dimnames = list(c(links$geneID),c(paste0("S",c(1:4)))))
+#' corMetGene(links, met, exp)
+corMetGene <- function(links, met, exp, file.out){
+
+    if(is.null(exp)) stop("Please set exp matrix")
+    if(is.null(met)) stop("Please set met matrix")
+    if(ncol(met) != ncol(exp)) stop("exp and met does not have the same size")
+
+    correlation.df <- plyr::adply(.data = links,
+                                  .margins = 1,
+                                  .fun = function(link){
+
+                                      idx <- which(rownames(exp) %in% link$geneID)
+
+                                      if(length(idx) < 1) {
+                                          return(tibble(p.value = NA, estimate_rho = NA))
+                                      }
+
+                                      exp <- log2(exp[idx,] + 1)
+                                      met <- met[link$regionID,]
+                                      res <- cor.test(exp,
+                                                      met,
+                                                      method = "spearman",
+                                                      exact = FALSE)
+                                      return(tibble(p.value = res$p.value,
+                                                    estimate_rho = res$estimate))
+                                  },.progress = "time")
+
+
+    correlation.df <- na.omit(correlation.df)
+    correlation.df$fdr <- p.adjust(correlation.df$p.value,method = "fdr")
+    if(!missing(file.out)) readr::write_tsv(x = correlation.df, path = file.out)
+
+    return(correlation.df)
 }

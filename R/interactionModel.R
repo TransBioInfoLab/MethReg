8776f53d1e1e0ecf4d7550aa094fa48b096cae67
Tiago Chedraoui Silva <tiagochst@gmail.com>
2020-03-23 16:35:33 -0400

    Update description

--- R/interactionModel.R
+++ R/interactionModel.R
@@ -1,11 +1,15 @@
 #' @title Calculate interaction linear model Target ~ TF + DNAm
 #' @description Evaluate interaction of DNA methylation,
 #' TF expression with target gene interaction using a linear model
-#' log2(RNA target ) ~ log2(TF) + DNAm + log2(TF) * DNAm
-#' @param met DNA methylation matrix
-#' @param exp gene EXpression matrix
-#' @param TF.target Dataframe with TF and target gene
-#' @param DNAm.target Dataframe with DNAm and target gene
+#' \deqn{log2(RNA target) ~ log2(TF) + DNAm + log2(TF) * DNAm}
+#'
+#' To consider covariates, RNA can also be the residuals.
+#' \deqn{log2(RNA target residuals) ~ log2(TF residual) + DNAm + log2(TF residual) * DNAm}
+#'
+#' @param triplet Dataframe with region (column name: regionID),
+#' TF  (column name: TF),  and target gene  (column name: target),
+#' @param dnam DNA methylation matrix  (columns: samples same order as met, rows: regions/probes)
+#' @param exp gene expression matrix (columns: samples same order as met, rows: genes)
 #' @return A dataframe with Region, TF, Estimates and Pvalue from linear model
 #' @examples
 #' \dontrun{
@@ -14,20 +18,39 @@
 #'  DNAm.target <- get_dnam_target_gene("hg38", method = "closest.gene")
 #' }
 #' @export
-interaction_model <- function(met,
-                              exp,
-                              TF.target,
-                              DNAm.target){
+interaction_model <- function(triplet,
+                              dnam,
+                              exp
+){
 
-    # changing RNA of target to log2
-    data$rna <- log2(data[[target]] + 1)
+    if(missing(met)) stop("Please set met argument with DNA methylation matrix")
+    if(missing(exp)) stop("Please set exp argument with gene expression matrix")
+    if(missing(triplet)) stop("Please set triplet argument with interactors (region,TF, target gene) data frame")
 
-    # transforming TF to log2 TF + 1
-    data[,tfs] <- log2(data[,tfs] + 1)
+    rna.target <- exp[rownames(exp) == triplet$target, , drop = FALSE]
+    met <- dnam[rownames(dnam) == as.character(triplet$regionID), ]
+    rna.tf <- exp[rownames(exp) == triplet$TF, , drop = FALSE]
 
+    data <- data.frame(
+        rna.target = log2(rna.target + 1) %>% as.numeric,
+        met = met %>% as.numeric,
+        rna.tf = log2(rna.tf + 1) %>% as.numeric
+    )
 
-    fmla.mod2 <- as.formula(paste("rna ~ ",  as.character(pairs[idx,2])))
-    mod2 <- lm(fmla.mod2, data = data.high)
-    return()
+    # 2) fit linear model: target RNA ~ DNAm + RNA TF
+    results <- lm (
+        rna.target ~ met + rna.target + rna.target * met,
+        data = data
+    )
+
+    results.pval <- summary(results.cases)$coefficients[-1, 4, drop = F] %>% t %>% as.data.frame()
+    colnames(results.pval) <- paste0("cases_pval_", colnames(results.pval))
+
+    results.estimate <- summary(results.cases)$coefficients[-1, 1, drop = F] %>% t %>% as.data.frame()
+    colnames(results.estimate) <- paste0("cases_estimate_", colnames(results.estimate))
+
+    out <- cbind(results.pval, results.estimate) %>% data.frame()
+
+    return(out)
 }
 

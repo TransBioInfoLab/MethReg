d32f5bd9487c1864652628f1381b2e78b2e214e6
Tiago <tiagochst@gmail.com>
2020-05-07 19:52:47 -0400

    Fixing namespace

--- R/utils.R
+++ R/utils.R
@@ -6,7 +6,7 @@
 #' @examples
 #' regions.names <- c("chr22:18267969-18268249","chr23:18267969-18268249")
 #' regions.gr <- make_granges_from_names(regions.names)
-#' @noRd
+#' @export
 make_granges_from_names <- function(names){
     names %>%
         data.frame %>%

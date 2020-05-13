97121921b1d8f271b8ce77c2f41c2be911dd29e6
Tiago <tiagochst@gmail.com>
2020-05-13 17:12:48 -0400

    Testing improved version

--- R/get_tf_in_region_hocomoco.R
+++ R/get_tf_in_region_hocomoco.R
@@ -38,10 +38,10 @@
 #' @param cores Number of cores to be used. Default 1.
 #' @importFrom Matrix colSums
 get_tf_in_region <- function(region,
-                            genome = c("hg19","hg38"),
-                            arrayType = c("450k","EPIC"),
-                            classification = c("subfamily","family"),
-                            cores = 1) {
+                             genome = c("hg19","hg38"),
+                             arrayType = c("450k","EPIC"),
+                             classification = c("subfamily","family"),
+                             cores = 1) {
 
     check_package("ELMER.data")
 
@@ -60,7 +60,16 @@ get_tf_in_region <- function(region,
     # get pre-computed data
     message("Loading pre-computed data...")
     motifs.probes <- get(data(list = paste0("Probes.motif.",genome,".",toupper(arrayType)),package = "ELMER.data",envir = environment()))
+
+    # motifs.probes.list <- plyr::alply (motifs.probes[,1:2],.margins = 2,function(colum){names(colum)[which(colum > 0)]})
+    # motifs.probes.df <- motifs.probes.list %>% plyr::ldply (data.frame)
+    # colnames(motifs.probes.df) <- c("motif","region")
+
     motifs.tfs <- get(data(list = paste0("TF.",classification),package = "ELMER.data",envir = environment()))
+    # motifs.tfs.df <- motifs.tfs %>% plyr::ldply (data.frame)
+    # colnames(motifs.tfs.df) <- c("motif","TF_external_gene_name")
+    # region.tf <- dplyr::full_join(motifs.probes.df,motifs.tfs.df)
+
 
     # Get probes regions for mapping the motifs
     probes.gr <- get_met_probes_info(genome = genome,arrayType = arrayType)
@@ -82,9 +91,158 @@ get_tf_in_region <- function(region,
         motifs <- colnames(motifs.probes)[Matrix::colSums(motifs.probes[probes,,drop = FALSE]) > 0]
         tfs <- unlist(motifs.tfs[motifs]) %>% as.character() %>% unique()
         tibble::tibble(region.names[x],tfs)
-    },.id = NULL,.progress = "time",.inform = TRUE,.parallel = .arallel)
+    },.id = NULL,.progress = "time",.inform = TRUE,.parallel = parallel)
 
     df$TF_id <- map_symbol_to_ensg(df$tfs)
     colnames(df) <- c("regionID","TF_external_gene_name","TF_ensembl_gene_id")
     return(df)
+}
+
+#' @title Get human TF list for a region using HOCOMOCO prediction
+#' @description Given a genomic region, this function obtains TFs that bind close to it (+-250bp).
+#' To this end, we used a pre-computed dataset for EPIC and HM450 Array that was created as follows:
+#' for each HOCOMOCO human TF, the motif was searched around the probe (+-250bp), and a binary matrix was created,
+#' with 1 if the motif was found, 0 if not.
+#' The function \code{get_tf_in_region} uses this pre-computed dataset to link regions to TFs:
+#' for each region, obtain the probes within it and a motif will be selected
+#' if at least one of the probes within the region has the motif (i.e. value 1 in the original dataset).
+#' Then for each TF motifs found within the region, we select the TFs within the same TF family/subfamily since they
+#' have similar binding motifs.
+#' @importFrom dplyr pull filter %>% tbl
+#' @importFrom plyr adply
+#' @importFrom sesameData sesameDataGet
+#' @importFrom GenomicRanges findOverlaps
+#' @importFrom methods is
+#' @importFrom utils data
+#' @return A dataframe with region, a TF name and TF gene ensembl ID
+#' @examples
+#' \dontrun{
+#'  regions.names <- c("chr1:60591-79592","chr4:40162197-43162198")
+#'  region.tf <- get_tf_in_region2(regions.names,
+#'                  genome = "hg19",
+#'                  arrayType = "450k",
+#'                  classification = "subfamily")
+#'
+#'  regions.gr <- make_granges_from_names(regions.names)
+#'  region.tf <- get_tf_in_region2(regions.gr,
+#'                  genome = "hg38",
+#'                  arrayType = "450k",
+#'                  classification = "family")
+#' }
+#' @export
+#' @param region Region to map. Either a Granges or a named vertor
+#' @param genome Human genome of reference "hg38" or "hg19"
+#' @param arrayType DNA methylation array "450k" or "EPIC"
+#' @param classification TF classification to be used.
+#' Either "subfamily" or "family".
+#' @param cores Number of cores to be used. Default 1.
+#' @importFrom Matrix colSums
+get_tf_in_region2 <- function(region,
+                              genome = c("hg19","hg38"),
+                              arrayType = c("450k","EPIC"),
+                              classification = c("subfamily","family"),
+                              cores = 1) {
+
+    check_package("ELMER.data")
+
+    arrayType <- match.arg(arrayType)
+    genome <- match.arg(genome)
+    classification <- match.arg(classification)
+
+    if(is(region,"character") | is(region,"factor")){
+        regions.gr <- make_granges_from_names(region)
+        region.names <- region
+    } else if(is(region,"GenomicRanges")){
+        regions.gr <- region
+        region.names <- make_names_from_granges(regions.gr)
+    }
+
+    # get pre-computed data
+    message("Loading pre-computed probes TFBS data")
+    motifs.probes <- get(data(list = paste0("Probes.motif.",genome,".",toupper(arrayType)),package = "ELMER.data",envir = environment()))
+    message("Transforming probes TFBS data to regions TFBS data")
+    motifs.probes <- map_motif_probes_to_regions(motifs.probes = motifs.probes,
+                                                 genome = genome,
+                                                 arrayType = arrayType,
+                                                 regions.gr = regions.gr)
+
+    message("Mapping regions to HOCOMOCO TFBS. This may take a while...")
+    parallel <- register_cores(cores)
+
+    message("Mapping regions to TFs")
+    motifs.tfs <- get(data(list = paste0("TF.",classification),package = "ELMER.data",envir = environment()))
+
+    # For each region/probe get the TFBS in region as a data.frame
+    motifs.probes.df <- plyr::alply(
+        colnames(motifs.probes),
+        .margins = 1,
+        function(colum.name){
+            colum <- motifs.probes[,colum.name,drop = TRUE]
+            regions <- rownames(colum)[which(colum > 0)]
+            tfs <- unlist(motifs.tfs[colum.name]) %>% as.character() %>% unique()
+            expand.grid(regions,tfs)
+        }, .progress = "time",.parallel = parallel)
+    motifs.probes.df <-
+    colnames(motifs.probes.df) <- c("motif","TF_external_gene_name")
+
+    message("Preparing output")
+    # Merge results of region and motif to motif TF
+
+    motifs.probes.df$TF_ensembl_gene_id <- map_symbol_to_ensg(df$TF_external_gene_name)
+    motifs.probes.df <- motifs.probes.df %>% na.omit
+    motifs.probes.df %>% unique
+}
+
+# Since motifs.probes is not region based, we need to
+# combine the rows of the sparce matrix to the overlapping regions
+map_motif_probes_to_regions <- function(motifs.probes,
+                                        genome,
+                                        arrayType,
+                                        regions.gr){
+
+    # Get probes regions for mapping the motifs
+    probes.gr <- get_met_probes_info(genome = genome,arrayType = arrayType)
+    probes.gr <- probes.gr[match(rownames(motifs.probes),names(probes.gr))]
+    regions.gr <- unique(regions.gr) # we cannot have duplicated regions
+
+    # Find which probes overlap with the regions
+    hits <- findOverlaps(regions.gr, probes.gr, ignore.strand = TRUE) %>% as.data.frame()
+    if(nrow(hits) == 0) stop("No overlap found between regions and DNA methylation array found")
+
+    region.with.more.than.one.probe <- unique(hits$queryHits[duplicated(hits$queryHits)])
+    unique.hits <- hits[!hits$queryHits %in% region.with.more.than.one.probe,]
+
+    motif.matrix <- NULL
+    # Do we have probes mapped to unique regions, if so copy probes and rename
+    # probes to regions
+    if(nrow(unique.hits) > 0){
+        motif.matrix <- motifs.probes[unique.hits$subjectHits,, drop = FALSE] %>% as.matrix()
+        rownames(motif.matrix) <- make_names_from_granges(regions.gr[unique.hits$queryHits])
+    }
+
+    # Do we have regions overlapping with multiple probes ?
+    if(length(region.with.more.than.one.probe) > 0){
+        non.unique.hits <- hits[hits$queryHits %in% region.with.more.than.one.probe,]
+        non.unique.motifs <- plyr::adply(unique(non.unique.hits$queryHits),1, function(x){
+            idx <- hits %>% filter(queryHits == x) %>% pull(subjectHits)
+            probes <- names(probes.gr)[idx]
+            Matrix::colSums(motifs.probes[probes,,drop = FALSE])
+        },.id = NULL,.progress = "time",.inform = TRUE)
+
+        rownames(non.unique.motifs) <- make_names_from_granges(regions.gr[unique(non.unique.hits$queryHits)])
+
+        if(is.null(motif.matrix)) {
+            motif.matrix <- non.unique.motifs
+        } else {
+            motif.matrix <-  rbind(motif.matrix, non.unique.motifs)
+        }
+    }
+
+    if(is(motifs.probes,"ngCMatrix")){
+        motif.matrix <-  motif.matrix %>% as.matrix() %>% as.data.frame()
+    }
+
+    # remove motifs not found in any regions
+    motif.matrix <- motif.matrix[,colSums(motif.matrix) > 0]
+    motif.matrix
 }
---
+++ tests/testthat/test-get_tf_in_region.R
@@ -0,0 +1,104 @@
+context("Map motif probes to regions")
+genome <- "hg38"
+arrayType <- "450k"
+motifs.probes <- get(
+    data(list = paste0("Probes.motif.",genome,".",toupper(arrayType)),
+         package = "ELMER.data",
+         envir = environment())
+)
+
+test_that("Mapping motifs only to regions overlapping single probes", {
+
+    regions.gr <- get_met_probes_info(genome = genome,arrayType = arrayType)
+    regions.gr <- regions.gr[!regions.gr$MASK_general]
+    regions.gr <- regions.gr[1:3]
+
+    motif.mapped <- map_motif_probes_to_regions(motifs.probes,
+                                                genome,
+                                                arrayType,
+                                                regions.gr)
+    expect_equal(nrow(motif.mapped),3)
+    expect_true(all(make_names_from_granges(regions.gr) %in% rownames(motif.mapped)))
+    expect_equal(motifs.probes[names(regions.gr)[1],],
+                 motif.mapped[make_names_from_granges(regions.gr[1]),])
+    expect_equal(motifs.probes[names(regions.gr)[2],],
+                 motif.mapped[make_names_from_granges(regions.gr[2]),])
+    expect_equal(motifs.probes[names(regions.gr)[3],],
+                 motif.mapped[make_names_from_granges(regions.gr[3]),])
+
+
+})
+
+test_that("Mapping motifs only to regions overlapping multiple probes", {
+
+    regions.gr <- get_met_probes_info(genome = genome,arrayType = arrayType)
+    regions.gr <- regions.gr[!regions.gr$MASK_general]
+    regions.gr <- reduce(regions.gr[5:10] + 500,ignore.strand =T)
+    # region 1: chr1 898303-899477
+    probes.region1 <- c("cg13938959","cg12445832","cg23999112")
+    # region 2: chr1 901656-903607
+    probes.region2 <- c("cg11527153","cg27573606","cg04195702")
+    motif.mapped <- map_motif_probes_to_regions(motifs.probes,
+                                                genome,
+                                                arrayType,
+                                                regions.gr)
+    expect_equal(nrow(motif.mapped),2)
+    expect_true(all(make_names_from_granges(regions.gr) %in% rownames(motif.mapped)))
+    expect_equal(motif.mapped[1,"AHR_HUMAN.H11MO.0.B"],
+                 sum(motifs.probes[probes.region1,"AHR_HUMAN.H11MO.0.B"])
+    )
+    expect_equal(motif.mapped[2,"GSC2_HUMAN.H11MO.0.D"],
+                 sum(motifs.probes[probes.region2,"GSC2_HUMAN.H11MO.0.D"])
+    )
+    expect_equal(motif.mapped[2,"SP1_HUMAN.H11MO.0.A"],
+                 sum(motifs.probes[probes.region2,"SP1_HUMAN.H11MO.0.A"])
+    )
+})
+
+
+test_that("Mapping motifs only to regions overlapping single/multiple probes", {
+
+    regions.gr <- get_met_probes_info(genome = genome,arrayType = arrayType)
+    regions.gr <- regions.gr[!regions.gr$MASK_general]
+    regions.gr <- c(reduce(regions.gr[5:10] + 500,ignore.strand =T),regions.gr[1:2])
+    # region 1: chr1 898303-899477
+    probes.region1 <- c("cg13938959","cg12445832","cg23999112")
+    # region 2: chr1 901656-903607
+    probes.region2 <- c("cg11527153","cg27573606","cg04195702")
+    motif.mapped <- map_motif_probes_to_regions(motifs.probes,
+                                                genome,
+                                                arrayType,
+                                                regions.gr)
+    expect_equal(nrow(motif.mapped),4)
+    expect_true(all(make_names_from_granges(regions.gr) %in% rownames(motif.mapped)))
+    expect_equal(motif.mapped["chr1:69591-69592","AHR_HUMAN.H11MO.0.B"],
+                 sum(motifs.probes["cg21870274","AHR_HUMAN.H11MO.0.B"])
+    )
+
+    expect_equal(motif.mapped["chr1:864703-864704","GSC2_HUMAN.H11MO.0.D"],
+                 sum(motifs.probes["cg08258224","GSC2_HUMAN.H11MO.0.D"])
+    )
+    expect_equal(motif.mapped["chr1:898303-899477","SP1_HUMAN.H11MO.0.A"],
+                 sum(motifs.probes[probes.region1,"SP1_HUMAN.H11MO.0.A"])
+    )
+
+    expect_equal(motif.mapped["chr1:901656-903607","SP1_HUMAN.H11MO.0.A"],
+                 sum(motifs.probes[probes.region2,"SP1_HUMAN.H11MO.0.A"])
+    )
+})
+
+
+test_that("No mapping throws an error", {
+    regions.gr <- get_met_probes_info(genome = genome,arrayType = arrayType)
+    regions.gr <- regions.gr[!regions.gr$MASK_general]
+
+    expect_error(
+        map_motif_probes_to_regions(motifs.probes,
+                                    genome,
+                                    arrayType,
+                                    shift(regions.gr[1],10))
+    )
+
+})
+
+

0ac5846f54d0a137b956c043cd6faf1fea2bf613
Tiago <tiagochst@gmail.com>
2020-05-14 00:14:27 -0400

    Improved version will replace old one

--- R/get_tf_in_region_hocomoco.R
+++ R/get_tf_in_region_hocomoco.R
@@ -38,106 +38,6 @@
 #' @param cores Number of cores to be used. Default 1.
 #' @importFrom Matrix colSums
 get_tf_in_region <- function(region,
-                             genome = c("hg19","hg38"),
-                             arrayType = c("450k","EPIC"),
-                             classification = c("subfamily","family"),
-                             cores = 1) {
-
-    check_package("ELMER.data")
-
-    arrayType <- match.arg(arrayType)
-    genome <- match.arg(genome)
-    classification <- match.arg(classification)
-
-    if(is(region,"character") | is(region,"factor")){
-        regions.gr <- make_granges_from_names(region)
-        region.names <- region
-    } else if(is(region,"GenomicRanges")){
-        regions.gr <- region
-        region.names <- make_names_from_granges(regions.gr)
-    }
-
-    # get pre-computed data
-    message("Loading pre-computed data...")
-    motifs.probes <- get(data(list = paste0("Probes.motif.",genome,".",toupper(arrayType)),package = "ELMER.data",envir = environment()))
-
-    # motifs.probes.list <- plyr::alply (motifs.probes[,1:2],.margins = 2,function(colum){names(colum)[which(colum > 0)]})
-    # motifs.probes.df <- motifs.probes.list %>% plyr::ldply (data.frame)
-    # colnames(motifs.probes.df) <- c("motif","region")
-
-    motifs.tfs <- get(data(list = paste0("TF.",classification),package = "ELMER.data",envir = environment()))
-    # motifs.tfs.df <- motifs.tfs %>% plyr::ldply (data.frame)
-    # colnames(motifs.tfs.df) <- c("motif","TF_external_gene_name")
-    # region.tf <- dplyr::full_join(motifs.probes.df,motifs.tfs.df)
-
-
-    # Get probes regions for mapping the motifs
-    probes.gr <- get_met_probes_info(genome = genome,arrayType = arrayType)
-    probes.gr <- probes.gr[names(probes.gr) %in% rownames(motifs.probes)]
-
-    # Find which probes overlap with the regions
-    hits <- findOverlaps(regions.gr,probes.gr) %>% as.data.frame()
-
-    if(nrow(hits) == 0) stop("No overlap found between regions and DNA methylation array found")
-
-    message("Mapping regions to HOCOMOCO TFBS, then to TF groups...")
-
-    parallel <- register_cores(cores)
-
-    # For each region overlapping get all probes overallping and their motifs
-    df <- plyr::adply(unique(hits$queryHits),1, function(x){
-        idx <- hits %>% filter(queryHits == x) %>% pull(subjectHits)
-        probes <- names(probes.gr)[idx]
-        motifs <- colnames(motifs.probes)[Matrix::colSums(motifs.probes[probes,,drop = FALSE]) > 0]
-        tfs <- unlist(motifs.tfs[motifs]) %>% as.character() %>% unique()
-        tibble::tibble(region.names[x],tfs)
-    },.id = NULL,.progress = "time",.inform = TRUE,.parallel = parallel)
-
-    df$TF_id <- map_symbol_to_ensg(df$tfs)
-    colnames(df) <- c("regionID","TF_external_gene_name","TF_ensembl_gene_id")
-    return(df)
-}
-
-#' @title Get human TF list for a region using HOCOMOCO prediction
-#' @description Given a genomic region, this function obtains TFs that bind close to it (+-250bp).
-#' To this end, we used a pre-computed dataset for EPIC and HM450 Array that was created as follows:
-#' for each HOCOMOCO human TF, the motif was searched around the probe (+-250bp), and a binary matrix was created,
-#' with 1 if the motif was found, 0 if not.
-#' The function \code{get_tf_in_region} uses this pre-computed dataset to link regions to TFs:
-#' for each region, obtain the probes within it and a motif will be selected
-#' if at least one of the probes within the region has the motif (i.e. value 1 in the original dataset).
-#' Then for each TF motifs found within the region, we select the TFs within the same TF family/subfamily since they
-#' have similar binding motifs.
-#' @importFrom dplyr pull filter %>% tbl
-#' @importFrom plyr adply
-#' @importFrom sesameData sesameDataGet
-#' @importFrom GenomicRanges findOverlaps
-#' @importFrom methods is
-#' @importFrom utils data
-#' @return A dataframe with region, a TF name and TF gene ensembl ID
-#' @examples
-#' \dontrun{
-#'  regions.names <- c("chr1:60591-79592","chr4:40162197-43162198")
-#'  region.tf <- get_tf_in_region2(regions.names,
-#'                  genome = "hg19",
-#'                  arrayType = "450k",
-#'                  classification = "subfamily")
-#'
-#'  regions.gr <- make_granges_from_names(regions.names)
-#'  region.tf <- get_tf_in_region2(regions.gr,
-#'                  genome = "hg38",
-#'                  arrayType = "450k",
-#'                  classification = "family")
-#' }
-#' @export
-#' @param region Region to map. Either a Granges or a named vertor
-#' @param genome Human genome of reference "hg38" or "hg19"
-#' @param arrayType DNA methylation array "450k" or "EPIC"
-#' @param classification TF classification to be used.
-#' Either "subfamily" or "family".
-#' @param cores Number of cores to be used. Default 1.
-#' @importFrom Matrix colSums
-get_tf_in_region2 <- function(region,
                               genome = c("hg19","hg38"),
                               arrayType = c("450k","EPIC"),
                               classification = c("subfamily","family"),
@@ -177,8 +77,8 @@ get_tf_in_region2 <- function(region,
         colnames(motifs.probes),
         .margins = 1,
         function(colum.name){
-            colum <- motifs.probes[,colum.name,drop = TRUE]
-            regions <- names(colum)[which(colum > 0)]
+            colum <- motifs.probes[,colum.name,drop = FALSE]
+            regions <- rownames(colum)[which(colum %>% pull > 0)];
             tfs <- unlist(motifs.tfs[colum.name]) %>% as.character() %>% unique()
             expand.grid(regions,tfs)
         }, .progress = "time",.parallel = parallel)
@@ -188,7 +88,7 @@ get_tf_in_region2 <- function(region,
     message("Preparing output")
     # Merge results of region and motif to motif TF
 
-    motifs.probes.df$TF_ensembl_gene_id <- map_symbol_to_ensg(df$TF_external_gene_name)
+    motifs.probes.df$TF_ensembl_gene_id <- map_symbol_to_ensg(motifs.probes.df$TF_external_gene_name)
     motifs.probes.df <- motifs.probes.df %>% na.omit
     motifs.probes.df %>% unique
 }

241f1daa17f82b12b82a2cc12f7e72efddd34273
Tiago Chedraoui Silva <tiagochst@gmail.com>
2021-05-27 15:55:09 -0400

    Remove example

--- R/plot_stratified_model.R
+++ R/plot_stratified_model.R
@@ -20,37 +20,6 @@
 #' and the following scatter plots: 1) TF vs DNAm, 2) Target vs DNAm,
 #' 3) Target vs TF, 4) Target vs TF for samples in Q1 and Q4 for DNA methylation,
 #' 5) Target vs DNAm for samples in Q1 and Q4 for the TF
-#' @examples
-#' library(dplyr)
-#' dnam <- runif(20,min = 0,max = 1) %>%
-#'   matrix(ncol = 1) %>%  t
-#' rownames(dnam) <- c("chr3:203727581-203728580")
-#' colnames(dnam) <- paste0("Samples",1:20)
-#'
-#' exp.target <-  runif(20,min = 0,max = 10) %>%
-#'   matrix(ncol = 1) %>%  t
-#' rownames(exp.target) <- c("ENSG00000232886")
-#' colnames(exp.target) <- paste0("Samples",1:20)
-#'
-#' exp.tf <- runif(20,min = 0,max = 10) %>%
-#'   matrix(ncol = 1) %>%  t
-#' rownames(exp.tf) <- c("ENSG00000232888")
-#' colnames(exp.tf) <- paste0("Samples",1:20)
-#'
-#' exp <- rbind(exp.tf, exp.target)
-#'
-#' triplet <- data.frame(
-#'    "regionID" =  c("chr3:203727581-203728580"),
-#'    "target" = "ENSG00000232886",
-#'    "TF" = "ENSG00000232888"
-#')
-#'
-#' results <- stratified_model(triplet = triplet,dnam = dnam, exp = exp)
-#' plots <- plot_stratified_model(
-#'     triplet.results = results,
-#'     dnam = dnam,
-#'     exp = exp
-#' )
 #' @importFrom ggpubr ggscatter ggarrange ggtexttable ttheme
 #' @importFrom ggplot2 xlab ylab geom_smooth
 #' @importFrom tibble as_tibble

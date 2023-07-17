41bb821a480dc8429444f3268c19bf45cb30a810
Tiago Silva <tiagosilva@Tiagos-MBP-2.hsd1.fl.comcast.net>
2023-07-17 18:41:15 -0400

    Adding missing documnetation

--- tests/testthat/test-create_triplet_distance_based.R
+++ tests/testthat/test-create_triplet_distance_based.R
@@ -1,23 +1,24 @@
 test_that("create_triplet_distance_based runs", {
-    triplet <- create_triplet_distance_based(
-        region = "chr3:189631389-189631389",
-        genome = "hg38",
-        target.method = "genes.promoter.overlap",
-        motif.search.window.size = 50,
-        motif.search.p.cutoff = 1
+  skip_on_bioc()
+  triplet <- create_triplet_distance_based(
+    region = "chr3:189631389-189631389",
+    genome = "hg38",
+    target.method = "genes.promoter.overlap",
+    motif.search.window.size = 50,
+    motif.search.p.cutoff = 1
+  )
+  expect_true("TP63" %in% triplet$target_symbol)
+  expect_true(
+    all(
+      c("regionID",
+        "target_symbol",
+        "target",
+        "TF_symbol",
+        "TF") %in% colnames(triplet)
     )
-    expect_true("TP63" %in% triplet$target_symbol)
-    expect_true(
-        all(
-            c("regionID",
-              "target_symbol",
-              "target",
-              "TF_symbol",
-              "TF") %in% colnames(triplet)
-        )
-    )
-    expect_type(triplet$TF_symbol,"character")
-    expect_type(triplet$target_symbol,"character")
+  )
+  expect_type(triplet$TF_symbol,"character")
+  expect_type(triplet$target_symbol,"character")
 })
 
 
